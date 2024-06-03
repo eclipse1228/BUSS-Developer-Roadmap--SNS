0a73731f7e18b7cb096b357ee6105c79d84a8026
All_right_Cool <ehrud3220@naver.com>
2024-06-04 08:56:40 +0900

    fix : fix the code

--- config/winston.js
+++ config/winston.js
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
     format: winston.format.json(),
     transports: [
         new winston.transports.File({ filename : 'logs/error.log', level : 'error'}),
-        new winston.transportsFile( {filename: 'logs/combined.log'} )
+        new winston.transports.File( {filename: 'logs/combined.log'} )
     ]
 });
 
