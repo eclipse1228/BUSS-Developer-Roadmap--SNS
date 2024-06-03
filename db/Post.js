07d90a2047bffe37967c26a98f0d113136546b38
M1nKyu <01.minkyu@gmail.com>
2024-06-03 12:37:50 +0900

    feat: Add Comment, Post Schema

---
+++ db/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', PostSchema);
