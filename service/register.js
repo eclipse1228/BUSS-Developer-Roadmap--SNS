6a9f38f1bdfe8aa1a1df3f9a4380a4060235e1eb
eokjae <cjdeh0904@naver.com>
2024-06-22 20:25:06 +0900

    feat: Add register profil

--- service/register.js
+++ service/register.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const User = require("../db/User");
 const bcrypt = require("bcryptjs");
+const fetch = require("node-fetch");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { id, pw, name, age, email, job } = req.body;
+  const { id, pw, name, age, email, job, githubUsername } = req.body;
 
   try {
     let user = await User.findOne({ id });
     if (user) {
       return res.status(400).json({ errors: [{ msg: "이미 존재하는 ID 입니다." }] });
+    }
+
+    let profileImageUrl = '/static/usericon.png'; // 기본 프로필 이미지 설정
+
+    if (githubUsername) {
+      try {
+        const response = await fetch(`https://api.github.com/users/${githubUsername}`);
+        if (response.ok) {
+          const githubData = await response.json();
+          profileImageUrl = githubData.avatar_url;
+        } else {
+          return res.status(400).json({ errors: [{ msg: "Invalid GitHub username" }] });
+        }
+      } catch (error) {
+        console.error("GitHub 사용자 정보를 가져오는 중 오류 발생:", error);
+        return res.status(500).json({ errors: [{ msg: "GitHub 사용자 정보를 가져오는 중 오류가 발생했습니다." }] });
+      }
     }
 
     user = new User({
@@ -19,7 +37,9 @@ router.post("/", async (req, res) => {
       name,
       age,
       email,
-      job
+      job,
+      githubUsername,
+      profileImageUrl
     });
 
     const salt = await bcrypt.genSalt(10);
